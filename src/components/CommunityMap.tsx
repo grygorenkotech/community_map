be907d22c76637f70655a22673c11fada8322089
grygorenkotech <artem@grygorenko.tech>
2025-08-13 17:05:06 +0300

    Update CommunityMap.tsx, added Bucha

--- src/components/CommunityMap.tsx
+++ src/components/CommunityMap.tsx
@@ -76,7 +76,8 @@ const cityCoordinates: { [key: string]: [number, number] } = {
   'Vic, Spain': [2.2544, 41.9301],
   'Wroclaw, Poland': [17.0385, 51.1079],
   'Gdansk, Poland': [18.6466, 54.3520],
-  'Valencia, Spain': [-0.3763, 39.4699]
+  'Valencia, Spain': [-0.3763, 39.4699],
+  'Bucha, Ukraine': [30.2120, 50.5435]
   };
 
 export const CommunityMap = () => {
